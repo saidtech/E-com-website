2c6aaad034a98b874e146e30ddc84d89576a4d90
Abdusaid <kakuza@live.se>
2022-11-03 14:16:58 +0100

    ashjdbahjudlsbiluadsbh

--- books.js
+++ books.js
@@ -1,4 +1,25 @@
-
+function renderBooks() {
+  const booksWrapper = document.querySelector()
+`<div class="book">
+<figure class="book__img--wrapper">
+    <img class="book__img" src="assets/atomic habits.jpg" alt="">
+  </figure>
+  <div class="book__title">
+    Atomic Habits
+  </div>
+  <div class="book__ratings">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+  </div>
+  <div class="book__price">
+    <span class="book__price--normal">$59.95</span> $14.95
+  </div>
+   </div>`
+  }
+renderBooks()
 
 // FAKE DATA
 function getBooks() {
